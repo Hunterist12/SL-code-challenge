360d8aa0dddacf0e6ee13acc2bbdc139a70ff569
Mason Hunter <masonhunter12@gmail.com>
2019-03-27 10:31:49 -0500

    Write all form logic except for validation

--- components/FormLogic.js
+++ components/FormLogic.js
@@ -1,40 +1,72 @@
-import { Formik } from "formik";
-import FormView from "./FormView";
+import React from 'react';
+import FormView from './FormView';
 
+class FormLogic extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      address2: '',
+      address: '',
+      errors: {},
+      touched: {},
+      isSubmitting: false
+    }
+  }
 
+  handleChange = (e) => {
+    const change = {};
+    change[e.target.name] = e.target.value;
+    this.setState(change);
+  }
 
-const FormLogic = () => {
-  return (
-    <Formik
-      component={FormView}
-      initialValues={{
-        firstName: "",
-        lastName: "",
-        address: "",
-        address2: ""
+  handleBlur = (e) => {
+
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const values = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      address: this.state.address,
+      address2: this.state.address2
+    };
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      this._resetForm();
+    }, 400);
+  }
+
+  _resetForm = () => {
+    this.setState({
+      firstName: '',
+      lastName: '',
+      address2: '',
+      address: '',
+      errors: {},
+      touched: {},
+      isSubmitting: false
+    });
+  }
+
+  render() {
+    return <FormView
+      handleSubmit={this.handleSubmit}
+      handleChange={this.handleChange}
+      handleBlur={this.handleBlur}
+      values={{
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        address: this.state.address,
+        address2: this.state.address2
       }}
-      validate={values => {
-        let errors = {};
-        if (!values.firstName) {
-          errors.firstName = "Required";
-        }
-        if (!values.lastName) {
-          errors.lastName = "Required";
-        }
-        if (!values.address) {
-          errors.address = "Required";
-        }
-        console.log(errors)
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
+      errors={this.state.errors}
+      touched={this.state.touched}
+      isSubmitting={this.state.isSubmitting}
     />
-  );
+  }
 };
 
 export default FormLogic;
